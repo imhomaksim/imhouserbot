a263367951c63241db32d501da1ee5d0c71e8906
imhomaksim <102488163+imhomaksim@users.noreply.github.com>
2023-05-25 17:21:37 +0300

    Update notes.py

--- imhobot/plugins/notes.py
+++ imhobot/plugins/notes.py
@@ -1,20 +1,5 @@
-#  Dragon-Userbot - telegram userbot
-#  Copyright (C) 2020-present Dragon Userbot Organization
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-from pyrogram import Client, filters, errors
+from pyrogram import Client, filters
+from plugins.settings.main_settings import module_list, file_list
 from pyrogram.types import (
     Message,
     InputMediaPhoto,
@@ -22,6 +7,13 @@ from pyrogram.types import (
     InputMediaAudio,
     InputMediaDocument,
 )
+
+from prefix import my_prefix
+prefix = my_prefix()
+
+module_list['notes'] = f'{prefix}example_edit'
+file_list['note'] = 'notes.py'
+
 
 from utils.db import db
 from utils.misc import modules_help, prefix
