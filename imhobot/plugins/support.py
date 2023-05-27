ebb95d8155b34c939d28e0ca510d5a5d567fd2da
imhomaksim <102488163+imhomaksim@users.noreply.github.com>
2023-05-27 12:52:34 +0300

    Update support.py

--- imhobot/plugins/support.py
+++ imhobot/plugins/support.py
@@ -2,25 +2,21 @@ from pyrogram import Client, filters
 from plugins.settings.main_settings import module_list, file_list
 
 from prefix import my_prefix
+
 prefix = my_prefix()
 
 
 @Client.on_message(filters.command('support', prefixes=prefix) & filters.me)
 async def support(client, message):
     await message.delete()
-  (
-      chat_id=message.chat.id,
-       
-        caption="
+    await client.send_photo(chat_id=message.chat.id,
+                             caption="""
 ██╗███╗░░░███╗██╗░░██╗░█████╗░██╗░░░██╗░██████╗███████╗██████╗░██████╗░░█████╗░████████╗
 ██║████╗░████║██║░░██║██╔══██╗██║░░░██║██╔════╝██╔════╝██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝
 ██║██╔████╔██║███████║██║░░██║██║░░░██║╚█████╗░█████╗░░██████╔╝██████╦╝██║░░██║░░░██║░░░
 ██║██║╚██╔╝██║██╔══██║██║░░██║██║░░░██║░╚═══██╗██╔══╝░░██╔══██╗██╔══██╗██║░░██║░░░██║░░░
 ██║██║░╚═╝░██║██║░░██║╚█████╔╝╚██████╔╝██████╔╝███████╗██║░░██║██████╦╝╚█████╔╝░░░██║░░░
-╚═╝╚═╝░░░░░╚═╝╚═╝░░╚═╝░╚════╝░░╚═════╝░╚═════╝░╚══════╝╚═╝░░╚═╝╚═════╝░░╚════╝░░░░╚═╝░░░"
-      caption="support @imhomaksim"
-    )
-
-
-module_list['Support'] = f'{prefix}support'
-file_list['Support'] = 'support.py'
+╚═╝╚═╝░░░░░╚═╝╚═╝░░╚═╝░╚════╝░░╚═════╝░╚═════╝░╚══════╝╚═╝░░╚═╝╚═════╝░░╚════╝░░░░╚═╝░░░""",
+                             caption="support @imhomaksim")
+    module_list['Support'] = f'{prefix}support'
+    file_list['Support'] = 'support.py'
