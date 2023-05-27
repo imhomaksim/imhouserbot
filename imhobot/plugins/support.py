ab8fd3af72403b811cae7a156475b394276a1a85
imhomaksim <102488163+imhomaksim@users.noreply.github.com>
2023-05-27 12:41:45 +0300

    Update support.py

--- imhobot/plugins/support.py
+++ imhobot/plugins/support.py
@@ -8,10 +8,17 @@ prefix = my_prefix()
 @Client.on_message(filters.command('support', prefixes=prefix) & filters.me)
 async def support(client, message):
     await message.delete()
-    await client.send_photo(
-        chat_id=message.chat.id,
-        photo="https://raw.githubusercontent.com/imhomaksim/imhouserbot/7f2351c8954db8521a82d5a3e7d74481bc45f985/imhobot/logo.png?token=GHSAT0AAAAAACCB3V466WROUWYM2OVR66B6ZDRZ24Q",
-        caption="Support: @imhomaksim"
+  (
+      chat_id=message.chat.id,
+       
+        caption="
+██╗███╗░░░███╗██╗░░██╗░█████╗░██╗░░░██╗░██████╗███████╗██████╗░██████╗░░█████╗░████████╗
+██║████╗░████║██║░░██║██╔══██╗██║░░░██║██╔════╝██╔════╝██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝
+██║██╔████╔██║███████║██║░░██║██║░░░██║╚█████╗░█████╗░░██████╔╝██████╦╝██║░░██║░░░██║░░░
+██║██║╚██╔╝██║██╔══██║██║░░██║██║░░░██║░╚═══██╗██╔══╝░░██╔══██╗██╔══██╗██║░░██║░░░██║░░░
+██║██║░╚═╝░██║██║░░██║╚█████╔╝╚██████╔╝██████╔╝███████╗██║░░██║██████╦╝╚█████╔╝░░░██║░░░
+╚═╝╚═╝░░░░░╚═╝╚═╝░░╚═╝░╚════╝░░╚═════╝░╚═════╝░╚══════╝╚═╝░░╚═╝╚═════╝░░╚════╝░░░░╚═╝░░░"
+      caption="support @imhomaksim"
     )
 
 
