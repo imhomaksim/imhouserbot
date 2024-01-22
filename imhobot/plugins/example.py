91850bd0fb41f42c21b1e31e9a3a872c4a01b04f
imhomaksim <102488163+imhomaksim@users.noreply.github.com>
2024-01-22 20:49:49 +0300

    Update example.py

--- imhobot/plugins/example.py
+++ imhobot/plugins/example.py
@@ -1,13 +1,18 @@
 from pyrogram import Client, filters
+from datetime import datetime
 from plugins.settings.main_settings import module_list, file_list
 
 from prefix import my_prefix
 prefix = my_prefix()
 
 
-@Client.on_message(filters.command("example_edit", prefixes=prefix) & filters.me)
-async def example_edit(client, message):
-    await message.edit("<code>This is an example module</code>")
+
+@Client.on_message(filters.command("time") & filters.me)
+async def time(client, message):
+    format_string = message.text.split(maxsplit=1)[1].strip() or "%d/%m/%Y %H:%M:%S"
+    now = datetime.datetime.now()
+    await message.reply_text(f"Сейчас {now.strftime(format_string)}")
+
 
 
 module_list['Example'] = f'{prefix}example_edit'
