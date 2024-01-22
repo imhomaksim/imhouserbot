d5346cec963bdcc6c3272e4b76c8ede5c396d2a9
imhomaksim <102488163+imhomaksim@users.noreply.github.com>
2024-01-22 20:52:24 +0300

    Update time_now.py

--- imhobot/plugins/time_now.py
+++ imhobot/plugins/time_now.py
@@ -6,26 +6,13 @@ from dateutil import tz
 from prefix import my_prefix
 prefix = my_prefix()
 
-SUPPORTED_LOCALES = {"en": "%d/%m/%Y\n%H:%M:%S", "ru": "%d.%m.%Y\n%H:%M:%S"}
 
-@Client.on_message(filters.command("time", prefixes=prefix) & filters.me)
+@Client.on_message(filters.command("time") & filters.me)
 async def time(client, message):
-    locale = message.from_user.language_code or "en"
-    format_string = SUPPORTED_LOCALES.get(locale)
+    format_string = message.text.split(maxsplit=1)[1].strip() or "%d/%m/%Y %H:%M:%S"
+    now = datetime.datetime.now()
+    await message.reply_text(f"Сейчас {now.strftime(format_string)}")
 
-    tz_name = message.text.split(maxsplit=1)[1].strip() or None
-    if tz_name:
-        try:
-            tz_obj = tz.gettz(tz_name)
-        except ZoneNotFoundError:
-            await message.reply_text(f"Invalid timezone: {tz_name}")
-            return
-
-    now = datetime.now(tz.UTC)
-    if tz_obj:
-        now = now.astimezone(tz_obj)
-
-    await message.edit(now.strftime(format_string))
 
 module_list['TimeNow'] = f'{prefix}time'
 file_list['TimeNow'] = 'time_now.py'
