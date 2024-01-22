c43f7d0fe45cbd552fc39b8c7a07e37f6966c202
imhomaksim <102488163+imhomaksim@users.noreply.github.com>
2024-01-22 21:19:44 +0300

    Update musicdl.py

--- imhobot/plugins/musicdl.py
+++ imhobot/plugins/musicdl.py
@@ -1,22 +1,21 @@
-from pyrogram import Client, filters
 from plugins.settings.main_settings import module_list, file_list
-
 from prefix import my_prefix
 prefix = my_prefix()
+import utils
 
 
 @Client.on_message(filters.command("mdl", prefixes=prefix) & filters.me)
 async def mdl(client, message):
     args = utils.get_args_raw(message)
     if not args:
-        await utils.answer(message, self.strings("args"))
+        await utils.answer(message, utils.strings("args"))
         return
 
-    message = await utils.answer(message, self.strings("loading"))
-    result = await self.musicdl.dl(args, only_document=True, source="soundcloud")
+    message = await utils.answer(message, utils.strings("loading"))
+    result = await self.musicdl.dl(args, only_document=True)
 
     if not result:
-        await utils.answer(message, self.strings("404").format(args))
+        await utils.answer(message, utils.strings("404").format(args))
         return
 
     await message.reply_document(
@@ -30,3 +29,4 @@ async def mdl(client, message):
 
 module_list['MusicDL'] = f'{prefix}mdl'
 file_list['MusicDL'] = 'musicdl.py'
+
